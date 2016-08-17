d80fcb1b0b89d9ee7c3ee86b19658f86de323652
Christopher Lee <sehlceris@gmail.com>
2016-08-17 02:33:33 -0700

    ElevationMapper implementation complete.

--- src/ElevationMapper.ts
+++ src/ElevationMapper.ts
@@ -7,15 +7,18 @@ import Constants from './Constants';
 
 const log:any = Utils.getLogger('ElevationMapper');
 
-const LOCATIONS_PER_REQUEST = 500;
-const DELAY_BETWEEN_REQUESTS = 2000;
+const LOCATIONS_PER_REQUEST = 128;
 const ELEVATION_JSON_KEY = 'elevation';
+const MAXIMUM_LAT_DIFF_FOR_MATCH = 0.0000000000001;
+const MAXIMUM_LONG_DIFF_FOR_MATCH = 0.000000000001;
 
 export default class ElevationMapper {
 
     spawnsJson:any;
     gmapsApiKey:String;
     gmapsClient:any;
+    segments:any;
+    segmentIndex:Number;
 
     constructor() {
         this.gmapsApiKey = Config.googleMapsApiKey;
@@ -26,33 +29,67 @@ export default class ElevationMapper {
     }
 
     execute():Promise {
-        return new Promise((resolve, reject) => {
-            let filteredSpawns = this.spawnsJson.filter((spawn) => {
-                return (typeof(spawn[ELEVATION_JSON_KEY]) !== 'number');
-            });
-            let segments = this.segmentLocations(filteredSpawns);
-            log.info(`segmented data into ${segments.length} requests...`);
 
-            let cumulativeDelay = 0;
-            let segmentPromises = segments.map((segment, i) => {
-                return new Promise(
-                    (resolve, reject) => {
-                        setTimeout(resolve, cumulativeDelay);
-                        cumulativeDelay += DELAY_BETWEEN_REQUESTS;
-                    })
-                    .then(() => {
-                        log.info(`populating segment ${i}`);
-                        return this.populateSegmentData(segment, i);
-                    });
+        let filteredSpawns = this.spawnsJson.filter((spawn) => {
+            return (typeof(spawn[ELEVATION_JSON_KEY]) !== 'number');
+        });
+
+        let segments = this.segmentLocations(filteredSpawns);
+        log.info(`segmented data into ${segments.length} requests...`);
+
+        this.segments = segments;
+        this.segmentIndex = 0;
+        return this.process()
+            .then(() => {
+                log.info(`all segments processed`);
+                log.info(`writing spawns json at ${Constants.SPAWNS_WITH_ELEVATIONS_JSON_PATH}`);
+                this.writeJson(this.spawnsJson);
+                log.info(`write complete`);
             });
 
-            Promise.all(segmentPromises)
-                .then(() => {
-                    log.info(`writing spawns json at ${Config.SPAWNS_WITH_ELEVATIONS_JSON_PATH}`);
-                    this.writeJson(this.spawnsJson);
-                    log.info(`write complete`);
-                })
-        })
+        // return new Promise((resolve, reject) => {
+        //
+        //     let cumulativeDelay = 0;
+        //     let segmentPromises = segments.map((segment, i) => {
+        //         return new Promise(
+        //             (resolve, reject) => {
+        //                 setTimeout(resolve, cumulativeDelay);
+        //                 cumulativeDelay += DELAY_BETWEEN_REQUESTS;
+        //             })
+        //             .then(() => {
+        //                 log.info(`populating segment ${i}`);
+        //                 return this.populateSegmentData(segment, i);
+        //             });
+        //     });
+        //
+        //     return Promise.all(segmentPromises)
+        //         .then(() => {
+        //             log.info(`writing spawns json at ${Constants.SPAWNS_WITH_ELEVATIONS_JSON_PATH}`);
+        //             this.writeJson(this.spawnsJson);
+        //             log.info(`write complete`);
+        //             return resolve();
+        //         })
+        //         .catch(reject);
+        // })
+    }
+
+    process():Promise {
+        log.info(`processing segment ${this.segmentIndex}`);
+        return this.populateSegmentData(this.segments[this.segmentIndex], this.segmentIndex)
+            .then(() => {
+                log.info(`processed segment ${this.segmentIndex}`);
+                if (this.segmentIndex < this.segments.length - 1) {
+                    this.segmentIndex++;
+                    return this.process();
+                }
+            })
+            .catch((err) => {
+                log.info(`failed to process segment ${this.segmentIndex}: ${err}; continuing...`);
+                if (this.segmentIndex < this.segments.length - 1) {
+                    this.segmentIndex++;
+                    return this.process();
+                }
+            })
     }
 
     segmentLocations(spawns:Array):Array<Array> {
@@ -80,18 +117,41 @@ export default class ElevationMapper {
 
         log.info(`making request for segment ${segmentIndex}`);
         try {
-            return this.gmapsClient.elevation({
-                locations: spawnLatLongs
+            return new Promise((resolve, reject) => {
+                this.gmapsClient.elevation({
+                    locations: spawnLatLongs
+                }, (err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    else if (result.json && result.json.results && result.json.results.length && result.json.status === 'OK') {
+                        return resolve(result.json.results);
+                    }
+                    else {
+                        return reject(`results are in an unrecognized format. ${JSON.stringify(result)}`)
+                    }
+                });
             })
-                .asPromise()
                 .then((results) => {
-                    log.info(`segment ${segmentIndex} elevation service returned results`);
-                    log.info(JSON.stringify(results));
+                    log.info(`segment ${segmentIndex} elevation service returned results. processing results, please wait... `);
+                    //log.info(JSON.stringify(results));
 
                     results.forEach((result) => {
                         if (result.elevation && result.location) {
                             let spawn = this.spawnsJson.find((spawn) => {
-                                return (spawn.lat === result.location.lat && spawn.lng === result.location.lng);
+
+                                if (typeof(spawn[ELEVATION_JSON_KEY]) === 'number') {
+                                    return false;
+                                }
+
+                                let diffLat = Math.abs(result.location.lat - spawn.lat);
+                                let diffLong = Math.abs(result.location.lng - spawn.lng);
+
+                                let match = ((diffLat <= MAXIMUM_LAT_DIFF_FOR_MATCH) && (diffLong <= MAXIMUM_LONG_DIFF_FOR_MATCH));
+
+                                //log.info(`diffLat: ${diffLat}; diffLong: ${diffLong}; match = ${match}`);
+
+                                return match
                             });
                             if (!spawn) {
                                 log.error(`segment ${segmentIndex} failed insert result for spawn at ${result.location.lat}, ${result.location.lng} - no match`);
@@ -101,13 +161,17 @@ export default class ElevationMapper {
                             }
                         }
                     });
+
+                    log.info(`processed results for segment ${segmentIndex}/${this.segments.length - 1}`);
                 })
                 .catch((err) => {
                     log.error(`segment ${segmentIndex} elevation service returned error: ${err}`);
                 });
         }
         catch (e) {
-            log.error(`caught exception while making request for segment ${segmentIndex}: ${e}`);
+            let message = `caught exception while making request for segment ${segmentIndex}: ${e}`;
+            log.error(message);
+            return Promise.reject(message);
         }
     }
 
@@ -124,5 +188,7 @@ mapper.execute()
     })
     .catch((err) => {
         log.error(`failed to write data: ${err}`);
-        process.exit(1);
+        setTimeout(() => {
+            process.exit(1);
+        }, 500);
     })
