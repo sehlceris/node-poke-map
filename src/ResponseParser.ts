8cbd6b2f94fec3e38d7eb5ba479c9118b1295906
Christopher Lee <sehlceris@gmail.com>
2016-08-18 13:08:39 -0700

    Added Int64 dependency, realized that the JSON response uses 64-bit integers

--- src/ResponseParser.ts
+++ src/ResponseParser.ts
@@ -1,10 +1,11 @@
 let moment = require('moment');
+let Int64 = require('node-int64')
 import fs = require('fs');
 import bluebird = require('bluebird');
 import Constants from './Constants';
 import Utils from './Utils';
-import Spawnpoint from "model/Spawnpoint";
-import Pokemon from "model/Pokemon";
+import Spawnpoint from "./model/Spawnpoint";
+import Pokemon from "./model/Pokemon";
 
 import TestData from "./TestData";
 
@@ -15,20 +16,23 @@ export default class ResponseParser {
 	static parsePokemon(pogobufMapResponse):Array<Pokemon> {
 		let wilds = pogobufMapResponse.wild_pokemons;
 		let pokes = wilds.map((wild) => {
-			let combinedEncounterId = String(wild.encounter_id.high) + String(wild.encounter_id.low);
-			let encodedEncounterId = Utils.btoa(combinedEncounterId);
-			log.info(`encounter id: ${encodedEncounterId}  ${combinedEncounterId}`);
+			let encounterIdInt64 = new Int64(wild.encounter_id.high, wild.encounter_id.low);
+			let encodedEncounterId = Utils.btoa(encounterIdInt64.toString());
+			log.info(`encounter id: ${encodedEncounterId}  ${encounterIdInt64}`);
+
+			let lastModifiedTimestampMsInt64 = new Int64(wild.last_modified_timestamp_ms.high, wild.last_modified_timestamp_ms.low);
 
 			let disappearTimeMs;
 			if (wild.time_till_hidden_ms > 0 && wild.time_till_hidden_ms < 3600000) {
-				let disappearTimeMs = wild.last_modified_timestamp_ms + wild.time_till_hidden_ms;
+				disappearTimeMs = lastModifiedTimestampMsInt64 + wild.time_till_hidden_ms;
 			}
 			else {
-				let disappearTimeMs = wild.last_modified_timestamp_ms + 900000;
+				disappearTimeMs = lastModifiedTimestampMsInt64 + 900000;
 			}
-			let disappearTime = moment.fromUtc(disappearTimeMs);
+			let disappearTime = moment(disappearTimeMs);
+			log.info(`disappear time: ${disappearTime} | ${disappearTimeMs}`);
 
-			let pkmn = new Pokemon({
+			let pkmnData = {
 				encounterId: encodedEncounterId,
 				spawnpointId: wild.spawn_point_id,
 				number: wild.pokemon_data.pokemon_id,
@@ -37,7 +41,12 @@ export default class ResponseParser {
 				long: wild.longitude,
 				lastModifiedTime: wild.last_modified_timestamp_ms,
 				timeUntilHidden: wild.time_till_hidden_ms
-			});
+			};
+			log.info(JSON.stringify(pkmnData));
+
+			let pkmn = new Pokemon(pkmnData);
+
+			return pkmn;
 		});
 
 		return pokes;
