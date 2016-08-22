5240f34831d037260502be053fae213164e9b5cd
Christopher Lee <sehlceris@gmail.com>
2016-08-22 16:35:26 -0700

    delete old commented code

--- src/model/Spawnpoint.ts
+++ src/model/Spawnpoint.ts
@@ -59,36 +59,6 @@ export default class Spawnpoint {
 				this.fireSpawn();
 			}, Utils.timestepTransformDown(Constants.HOUR), true);
 		}, Utils.timestepTransformDown(firstFireDelay));
-
-		// let spawnTimeout = this.startTime - new Date(); //time diff from now that spawn will occur
-		// let firstFireDelay = spawnTimeout; //timeout for when the first spawn will be fired and the hour timer started
-		//
-		// //If the spawn is in the past...
-		// if (firstFireDelay <= 0) {
-		//
-		//     //If the spawn occurred less than N minutes ago, fire the spawn immediately...
-		//     if (firstFireDelay < 0 && firstFireDelay >= (0 - (Config.spawnpointLookbackMinutes * Constants.MINUTE))) {
-		//         setTimeout(() => {
-		//             log.debug(`${this.id} fired ${(60 - Math.abs(firstFireDelay / Constants.MINUTE)).toFixed(1)} min in the past, firing spawn immediately`);
-		//             this.fireSpawn();
-		//         }, 0);
-		//     }
-		//
-		//     //...and then add an hour to the delay
-		//     firstFireDelay += Constants.HOUR;
-		// }
-		//
-		// log.debug(`${this.id} first fire in ${(firstFireDelay / Constants.MINUTE).toFixed(1)} min`);
-		//
-		// //Timeout to trigger spawn for the first time
-		// this.timerStartTimeout = setTimeout(() => {
-		//     this.fireSpawn();
-		//
-		//     //Start an interval timer to trigger spawn each hour
-		//     this.timerInterval = setInterval(() => {
-		//         this.fireSpawn();
-		//     }, Utils.timestepTransformDown(Constants.HOUR));
-		// }, Utils.timestepTransformDown(firstFireDelay));
 	}
 
 	stopSpawnTimer():void {
