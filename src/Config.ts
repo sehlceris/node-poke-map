9ca38949c643f1c7d8c0375ed99519e7bd04dd64
Christopher Lee <sehlceris@gmail.com>
2016-08-26 00:30:48 -0700

    Config now reloads itself every so often

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,13 @@
 let fs = require('fs');
+import Utils from './Utils';
 import Constants from './Constants';
 
 export interface Config {
     //End user will probably want to configure these
+    scanCenterLat:number; // lat number describing scan center
+    scanCenterLong:number; // long number describing scan center
+    scanRadiusMeters:number; // radius of scan. spawnpoints outside this radius will not be scanned
+
     workerScanDelayMs:number; // delay each individual worker must wait before scanning again
     globalScanDelayMs:number; // delay between sending scan requests globally (includes all workers)
     spawnScanDelay:number; // delay between a spawn occurring and the scanner trying to pick it up
@@ -10,10 +15,9 @@ export interface Config {
     workerMaximumMovementSpeedMetersPerSecond:number; //maximum speed a worker may move in order to reach a spawn
     workerMaximumLoggedInTime:number; //maximum amount of time a worker may be logged in before needing to relogin
     maximumRequestQueueLength:number; // maximum number of requests that can be queued before dropping
-    scanCenterLat:number; // lat number describing scan center
-    scanCenterLong:number; // long number describing scan center
-    scanRadiusMeters:number; // radius of scan. spawnpoints outside this radius will not be scanned
     maxAccounts:number; // app will not use more than this number of accounts, despite however many are available in the workers.json
+
+    restPort:number; // express server REST API Port
     googleMapsApiKey:string; // your google maps API key
 
     //MongoDB
@@ -23,21 +27,16 @@ export interface Config {
     mongoDbPort:number;
     mongoDbDatabaseName:string;
 
-    //Express
-    restPort:number;
-
-    //Health Checking
-    healthCheckInterval:number; //interval at which health checking is performed
-    globalMaximumBannedAccountsLimit:number; //maximum number of banned accounts detected before the program exits
-
     //End user probably should not touch
+    globalMaximumBannedAccountsLimit:number; //maximum number of banned accounts detected before the program exits
     workerConsecutiveLoginFailureLimit:number; //maximum number of consecutive failed worker login attempts before worker is removed from pool
     workerConsecutiveScanFailureLimit:number; //maximum number of consecutive failed worker scan attempts before worker is removed from pool
-    randomWorkerLoginFuzzFactor:number; // random delay until the worker is allowed to log in.
-    randomMaximumLoggedInTimeFuzzFactor:number; // random amount of time to subtract from workerMaximumLoggedInTime
     workerReloginDelayMs:number; //time to wait before attempting to log worker in again, if login fails
     enableGreedyWorkerAllocation:boolean; // will use all provided workers equally instead of trying to maximize each worker's usage
     enableParallelRequests:boolean; // if false, will wait for previous scan request to finish before allowing start of next. probably best to set it to true unless you want to throttle this way
+
+    randomWorkerLoginFuzzFactor:number; // random delay until the worker is allowed to log in.
+    randomMaximumLoggedInTimeFuzzFactor:number; // random amount of time to subtract from workerMaximumLoggedInTime
     randomWorkerDelayFuzzFactor:number; // random worker delay added to the regular scan delay. between 0 and this number, in milliseconds
     randomGlobalScanDelayFuzzFactor:number; // random global scan delay added to the regular scan delay. between 0 and this number, in milliseconds
     randomLatFuzzFactor:number; // random latitude fuzz. between 0 and this number
@@ -46,11 +45,13 @@ export interface Config {
     latLongDecimalPlaces:number; // how many decimal places remain at the end of lat/longs when submitting to the server
     elevDecimalPlaces:number; // how many decimal places remain at the end of elevations when submitting to the server
 
-    //Logging, statistics, and debugging
     consoleLogLevel:String; // 'debug', 'verbose', 'info', 'warn', 'error'
     fileLogLevel:String; // 'debug', 'verbose', 'info', 'warn', 'error'
     logFilePath:String; // path you want the file log to be output to
+
     statisticLoggingInterval:number; // interval, in milliseconds, to print statistics. you probably should set it to at least 60000
+    healthCheckInterval:number; //interval at which health checking is performed
+    configReloadInterval:number; //interval at which config will be reloaded (in case of updates to the config are desired without restarting the server) - set to 0 to disable
 
     //Simulation
     simulate:boolean; // if true, requests will NOT be made to the API and instead a simulation will occur. useful for determining proper settings for workers/scan delay
@@ -82,4 +83,28 @@ export interface SlackBotConfig {
 
 let config = JSON.parse(fs.readFileSync(Constants.CONFIG_JSON_PATH, 'utf-8'));
 
+//set up an infinite loop to reload the config every once in a while
+let reloadConfig = function () {
+    fs.readFile(Constants.CONFIG_JSON_PATH, 'utf-8', (err, data) => {
+        try {
+            if (err) {
+                throw err;
+            }
+            config = JSON.parse(data);
+            console.log(`reloaded config`);
+        }
+        catch (e) {
+            console.log(`failed to reload config: ${config}`);
+        }
+    });
+    setTimeout(reloadConfig, config.configReloadInterval);
+};
+if (config.configReloadInterval > 4999) {
+    setTimeout(reloadConfig, config.configReloadInterval);
+}
+else {
+    config.configReloadInterval = 0;
+}
+
 export default config;
+console.log(`loaded config - reload interval is ${config.configReloadInterval}`); //console.log because the config has not been loaded yet
